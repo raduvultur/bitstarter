cc41e57f3f77178b3d6437025392de75da17e152
raduvultur <raduvultur@gmail.com>
2013-07-10 22:03:51 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World3!');
+  response.send(readHtml("index.html"));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+function readHtml(filename){
+  var filebuff = fs.readFileSync(filename);
+  return filebuff.toString();
+}
